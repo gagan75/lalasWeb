b76652f12a891578a8c6cc513f086c16358a24e9
gagan <gagan.ch1997@gmail.com>
2020-08-11 10:13:42 +0530

    Updates

---
+++ precache-manifest.d553bc615ea2639b8b0f2ab186fcd88b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc49d8d0b54996dfc21ca532f162268",
+    "url": "/lalas-web2/index.html"
+  },
+  {
+    "revision": "177592f525d052b20767",
+    "url": "/lalas-web2/static/css/2.8cdcd3d5.chunk.css"
+  },
+  {
+    "revision": "6d8d8ecf96c1edec94e1",
+    "url": "/lalas-web2/static/css/main.f81f385a.chunk.css"
+  },
+  {
+    "revision": "177592f525d052b20767",
+    "url": "/lalas-web2/static/js/2.5f5d3b59.chunk.js"
+  },
+  {
+    "revision": "59bd243ce0ef79496913accf707fcc48",
+    "url": "/lalas-web2/static/js/2.5f5d3b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8d8ecf96c1edec94e1",
+    "url": "/lalas-web2/static/js/main.5fd611ae.chunk.js"
+  },
+  {
+    "revision": "fa9d1d23afafa1a4692a",
+    "url": "/lalas-web2/static/js/runtime-main.a21fb0f0.js"
+  },
+  {
+    "revision": "e0e038618d0955f57def7005719e5a50",
+    "url": "/lalas-web2/static/media/angle-down-solid.e0e03861.svg"
+  },
+  {
+    "revision": "fcc55fdeece27589871e580ad82a5bae",
+    "url": "/lalas-web2/static/media/angle-down-solid1.fcc55fde.svg"
+  },
+  {
+    "revision": "aa492f0fda8cb409d16ccc9704f467c5",
+    "url": "/lalas-web2/static/media/bg-pagetitle.aa492f0f.jpg"
+  },
+  {
+    "revision": "0e1ab56808b2caf63e89fe420be5752a",
+    "url": "/lalas-web2/static/media/bg_menu2.0e1ab568.jpg"
+  },
+  {
+    "revision": "9df8aff0bf6d50d008adbf2ee0197be8",
+    "url": "/lalas-web2/static/media/img1.9df8aff0.jpg"
+  },
+  {
+    "revision": "35297cb341f6ff6769c293e9bba47fd0",
+    "url": "/lalas-web2/static/media/img2.35297cb3.jpg"
+  },
+  {
+    "revision": "833a00a8d4a5f929955bb0c19e961265",
+    "url": "/lalas-web2/static/media/img3.833a00a8.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lalas-web2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/lalas-web2/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/lalas-web2/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/lalas-web2/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/lalas-web2/static/media/slick.f97e3bbf.svg"
+  }
+]);
